4dc3132de2bf8e7522c69d62d6cb6fe7f1899a47
Rajeev Ranjan <135375413+rjranjan2112@users.noreply.github.com>
2023-08-25 19:08:23 +0530

    Basic Python Programming

---
+++ 01_05_Intro.py
@@ -0,0 +1,27 @@
+# print("Hello People")
+# print(5)
+# print(3)
+# print("Hello Python Learner"
+
+
+# print(142*143)
+
+# To create a new line 
+
+print("Hello this is Rajeev ranjan\nAnd I am learning Python Programming languagee right now ")
+
+# For multiline comment use single coat or double coat  =>  '''    '''  or  """     """
+
+''' a = 13
+    b = 2144
+    c = a + b 
+    print (c) 
+''' 
+
+harry = 9 
+c = "harry" 
+print(c)
+
+harry = 9 
+c = harry
+print(c)
---
+++ 06_Variables.py
@@ -0,0 +1,30 @@
+a = 123
+print(a)
+
+a1 = 13
+b = "Rajeev"
+
+c = a + a1 
+print(c)
+
+d = True 
+e = None
+f = 1.563
+g = complex(8,2)
+
+print("The type of a is", type(a)) # type int 
+
+print("The type of a is", type(b)) # type str 
+
+print("The type of a is", type(c)) # type int 
+
+print("The type of a is", type(d)) # type bool
+
+print("The type of a is", type(e)) # type Nonetype 
+
+print("The type of a is", type(f)) # type float
+
+print("The type of a is", type(g)) # type complex
+
+
+
